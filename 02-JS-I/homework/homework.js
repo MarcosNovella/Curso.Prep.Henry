d3442b0d83b621f83d0b6a45fa5f5fcfb07a67cd
MarcosNovella <marcosnovella99@gmail.com>
2022-08-09 20:17:30 -0300

    git commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,7 +28,7 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código: 
-  return 'str';
+  return str;
   
 }
 
@@ -36,26 +36,26 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  return suma(x + y)
+  return x + y
 
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  return resta(x - y)
+  return (x - y)
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
-  
+  return x * y 
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-  
+  return x / y 
 }
 
 function sonIguales(x, y) {
@@ -63,6 +63,7 @@ function sonIguales(x, y) {
   // De lo contrario, devuelve "false"
   // Tu código:
   
+
 }
 
 function tienenMismaLongitud(str1, str2) {
